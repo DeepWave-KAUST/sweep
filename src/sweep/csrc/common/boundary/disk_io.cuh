ae75043dd2689bcc91368657544d8eb906467cd5
shaowen.wang <shaowinw@163.com>
2026-05-05 02:41:38 +0300

    Gate parallel 3D disk writes by chunk size

--- src/sweep/csrc/common/boundary/disk_io.cuh
+++ src/sweep/csrc/common/boundary/disk_io.cuh
@@ -143,6 +143,19 @@ inline void write_boundary_disk_2d_task(const BoundaryDisk2DWriteTask& task)
 
 inline void write_boundary_disk_3d_task(const BoundaryDisk3DWriteTask& task)
 {
+    constexpr size_t kParallelWriteThresholdBytes = 8 * 1024 * 1024;
+    size_t write_bytes =
+        2 * (task.top_elems + task.front_elems + task.left_elems) * sizeof(float);
+    if (write_bytes < kParallelWriteThresholdBytes) {
+        write_boundary_file_chunk(task.paths[0], task.top_offset, task.top.data(), task.top_elems);
+        write_boundary_file_chunk(task.paths[1], task.top_offset, task.bottom.data(), task.top_elems);
+        write_boundary_file_chunk(task.paths[2], task.front_offset, task.front.data(), task.front_elems);
+        write_boundary_file_chunk(task.paths[3], task.front_offset, task.back.data(), task.front_elems);
+        write_boundary_file_chunk(task.paths[4], task.left_offset, task.left.data(), task.left_elems);
+        write_boundary_file_chunk(task.paths[5], task.left_offset, task.right.data(), task.left_elems);
+        return;
+    }
+
     std::exception_ptr write_error = nullptr;
     std::mutex write_error_mutex;
     auto write_one = [&](std::string path, size_t offset, const float* data, size_t elems) {
